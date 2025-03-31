67f529193bdbf2961e3c978bf84241de3e6145ef
Tai <bintolo1b@gmail.com>
2025-03-31 22:13:14 +0700

    merge fetch order infor api

--- mobile_shop_project/mobile_shop_project/settings.py
+++ mobile_shop_project/mobile_shop_project/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'mobile_shop_django',
         'USER': 'root',
-        'PASSWORD': 'bulletsilver',
+        'PASSWORD': 'Trantai25022004',
         'HOST': 'localhost',
         'PORT': '3306',
     }
